3c8b254dce53583de71b90b0b12be2b970f497ca
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-05-11 14:59:27 -0500

    Accordion using useState, Search beginnings using useEffect

---
+++ widgets/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Accordion from './components/Accordion'
+import Search from './components/Search'
+
+// const items = [{ title: 'What is React?', content: 'React is a front end javascript frameowrk' }, { title: 'Why us React?', content: 'React is a favourite framework for devs' }, { title: 'How do you use React?', content: 'You  use React by creating components' }]
+
+const App = () => {
+    return (
+        <div>
+            {/* <Accordion items={items} /> */}
+            <Search />
+        </div>)
+}
+export default App
