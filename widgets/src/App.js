fdf0ab9b6cdd1680c0e26a90048be05337b62b00
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-05-12 11:33:47 -0500

    search feature has functionality. adding dropdown feature

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Dropdown from './components/Dropdown';
 // import Accordion from './components/Accordion'
-import Search from './components/Search'
+// import Search from './components/Search'
 
 // const items = [{ title: 'What is React?', content: 'React is a front end javascript frameowrk' }, { title: 'Why us React?', content: 'React is a favourite framework for devs' }, { title: 'How do you use React?', content: 'You  use React by creating components' }]
-
+const options = [{label: 'The Colour Red', value: 'red'}, {label: 'The Colour Green', value: 'green'}, {label: 'A Shade Blue', value: 'blue'}]
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div>
             {/* <Accordion items={items} /> */}
-            <Search />
+            {/* <Search /> */}
+            <Dropdown
+            onSelectedChange={setSelected}
+            selected={selected}
+            options={options}/>
         </div>)
 }
 export default App
