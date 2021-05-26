b52639e83720c44a9b6efd934bcd4b517b0dd89c
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-05-26 11:22:34 -0500

    useVideos custom hook working in App

--- videos-hooks/src/hooks/useVideos.js
+++ videos-hooks/src/hooks/useVideos.js
@@ -4,17 +4,17 @@ import youtube from '../apis/youtube'
 const useVideos = (defaultSearchTerm) => {
     const [videos, setVideos] = useState([]);
     useEffect(() => {
-        onTermSubmit(defaultSearchTerm);
-    }, []);
-    const onTermSubmit = async (term) => {
+        search(defaultSearchTerm);
+    }, [defaultSearchTerm]);
+    const search = async (term) => {
         const response = await youtube.get('/search', {
             params: {
                 q: term,
             },
         });
         setVideos(response.data.items);
-
     }
+    return { videos, search }
 }
 
 export default useVideos
