ff6153d6eb70c0184443fc82401523a2bd94610e
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-05-27 16:31:55 -0500

    saving before leaving for the day, in the middle of mapping through song list

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -3,13 +3,13 @@ import { connect } from 'react-redux'
 
 class SongList extends Component {
   render() {
+    console.log(this.props)
     return <div>Song List</div>;
   }
 }
 
 const mapStateToProps = (state) => {
-  console.log(state);
-  return state;
+  return { songs: state.songs };
 }
 
 export default connect(mapStateToProps)(SongList)
