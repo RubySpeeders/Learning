4236f826fb0016077db934f2fbad6c1a93e2159c
Sarah Peters <69406113+RubySpeeders@users.noreply.github.com>
2021-05-27 16:27:21 -0500

    fixing semantic ui href, configuring Provider and connect and mapStateToProps

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,20 @@
+import { combineReducers } from 'redux'
+
+const songs = () => {
+  return [{ title: 'No Scrubs', duration: '4:05' },
+  { title: 'Macarena', duration: '2:30' },
+  { title: 'All Star', duration: '3:15' },
+  { title: 'I want It That Way', duration: '1:45' }]
+}
+
+const selectedSongs = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload
+  }
+  return selectedSong
+}
+
+export default combineReducers({
+  songs,
+  selectedSongs
+})
